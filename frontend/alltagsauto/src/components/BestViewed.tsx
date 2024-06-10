37bbbac208b3cf48b43574ed65daf4bb545848b8
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-10 15:04:34 +0200

    responsive fix bestViewed 2

--- frontend/alltagsauto/src/components/BestViewed.tsx
+++ frontend/alltagsauto/src/components/BestViewed.tsx
@@ -5,8 +5,8 @@ export default function BestViewed() {
   console.log(carsExample);
 
   return (
-    <div className=" h-3/4  items-center justify-center  p-10 flex flex-row lg:gap-4 gap-10 bg-gray-700 text-customWhite  flex-wrap ">
-      <div className=" flex flex-col lg:w-1/3 flex-wrap">
+    <div className=" h-3/4  items-center justify-center  p-10 flex flex-row lg:gap-4 gap-10 bg-gray-700 text-customWhite flex-wrap  lg:flex-nowrap ">
+      <div className=" flex flex-col  lg:w-1/3 flex-wrap">
         <h2 className="font-bold text-4xl">Am häufigsten angesehen</h2>
         <p>Sie sind möglicherweise am gefragtesten</p>
       </div>
