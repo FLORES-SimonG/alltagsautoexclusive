faec8bee548e1ba9d20137feaf8dca845e6ca980
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-12 21:37:12 +0200

    problem with bestviewed

--- frontend/alltagsauto/src/components/BestViewed.tsx
+++ frontend/alltagsauto/src/components/BestViewed.tsx
@@ -13,7 +13,7 @@ export default function BestViewed() {
     <div className=" h-3/4  items-center justify-center  p-10 flex flex-row lg:gap-4 gap-10 bg-gray-700 flex-wrap  lg:flex-nowrap ">
       <div className=" flex flex-col  lg:w-1/3 flex-wrap text-customWhite ">
         <h2 className="font-bold text-4xl ">Am häufigsten angesehen</h2>
-        <p>Sie sind möglicherweise am gefragtesten</p>
+        <p>Sie sind möglicherweise am gefragtesten.</p>
       </div>
       <div className="lg:w-2/3  flex flex-row justify-center gap-10 flex-wrap ">
         {sortedCarsClick.map((car, index) => {
