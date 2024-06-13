730c44f19ff396a3f87ca483c4b27a207acc05fd
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-13 15:36:02 +0200

    added styles&more

--- frontend/alltagsauto/src/components/Navbar.tsx
+++ frontend/alltagsauto/src/components/Navbar.tsx
@@ -3,30 +3,44 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className="bg-gray-800 px-8 py-3 flex flex-row justify-between items-center">
+      <a href="#">
+
       <Image src="/logo.svg" alt="logo" width={200} height={200} />
+      </a>
       <ul className="flex justify-center gap-10">
         <li>
-          <a href="#" className="text-white">
+          <a
+            href="#"
+            className="text-customWhite hover:text-secondary/95 transition-colors"
+          >
             Home
           </a>
         </li>
         <li>
-          <a href="#" className="text-white">
+          <a
+            href="#"
+            className="text-customWhite hover:text-secondary/95 transition-colors"
+          >
             Über uns
           </a>
         </li>
         <li>
-          <a href="#" className="text-white">
+          <a
+            href="#"
+            className="text-customWhite hover:text-secondary/95 transition-colors"
+          >
             Autos
           </a>
         </li>
         {/* <li>
-            <a href="#" className="text-white">
+            <a href="#" className="text-customWhite hover:text-secondary/95 transition-colors">
               Contact
             </a>
           </li> */}
       </ul>
-      <button className="text-white flex py-3 px-10 justify-center items-center gap-8 border-white border-2 rounded-lg">Anmelden</button>
+      <button className="text-white flex py-3 px-10 justify-center items-center gap-8 border-white border-2 rounded-lg hover:border-tertiary transition-colors">
+        Anmelden
+      </button>
     </nav>
   );
 }
