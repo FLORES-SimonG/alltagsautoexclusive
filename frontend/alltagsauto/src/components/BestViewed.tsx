4aa5ac7f8157a8b7ddfa2b47faafbc0a9d9d0eee
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-13 16:52:12 +0200

    added buttons

--- frontend/alltagsauto/src/components/BestViewed.tsx
+++ frontend/alltagsauto/src/components/BestViewed.tsx
@@ -2,6 +2,7 @@ import carsExample, { sortedCarsByClicks } from "@/helpers/carsExample";
 import { Card } from "flowbite-react";
 import Image from "next/image";
 import CardProduct from "./CardProduct";
+import ButtonPrimary from "./buttons/ButtonPrimary";
 // import {sortedCarsById} from "@/helpers/carsExample";
 
 
@@ -14,6 +15,9 @@ export default function BestViewed() {
       <div className=" flex flex-col  lg:w-1/3 flex-wrap text-customWhite ">
         <h2 className="font-bold text-4xl ">Am häufigsten angesehen</h2>
         <p>Sie sind möglicherweise am gefragtesten.</p>
+        <div className=" flex justify-center m-6">
+          <ButtonPrimary title="Mehr sehen" size="sm" color="bg-indigo-700" customHover="bg-indigo-600"/>
+        </div>
       </div>
       <div className="lg:w-2/3  flex flex-row justify-center gap-10 flex-wrap ">
         {sortedCarsClick.map((car, index) => {
--- frontend/alltagsauto/src/components/NewCars.tsx
+++ frontend/alltagsauto/src/components/NewCars.tsx
@@ -2,22 +2,20 @@ import carsExample from "@/helpers/carsExample";
 import { Card } from "flowbite-react";
 import Image from "next/image";
 import CardProduct from "./CardProduct";
-import {sortedCarsById} from "@/helpers/carsExample";
-
-
+import { sortedCarsById } from "@/helpers/carsExample";
+import ButtonPrimary from "./buttons/ButtonPrimary";
 
 export default function NewCars() {
   // console.log(carsExample);
   const sortedCars = sortedCarsById(carsExample);
   return (
     <div className=" h-3/4  items-center justify-center  p-10 flex flex-row lg:gap-4 gap-10 bg-gray-600 flex-wrap  lg:flex-nowrap ">
-     
       <div className="lg:w-3/5  flex flex-row justify-start gap-10 flex-wrap ">
         {sortedCars.map((car, index) => {
           if (index < 2) {
             return (
               <CardProduct
-              key={car.id}
+                key={car.id}
                 image={car.mainImage}
                 price={car.pricePerDayCHF}
                 transmission={car.transmission}
@@ -29,9 +27,16 @@ export default function NewCars() {
           }
         })}
       </div>
-      <div className=" flex flex-col  lg:w-1/3 flex-wrap text-customWhite ">
+
+      <div className=" flex flex-col  lg:w-1/3 flex-wrap text-customWhite  ">
         <h2 className="font-bold text-4xl ">Die letzte Neuheit</h2>
-        <p>Verpassen Sie nicht die neuesten und exklusivsten Angebote in unserem Katalog!</p>
+        <p>
+          Verpassen Sie nicht die neuesten und exklusivsten Angebote in unserem
+          Katalog!
+        </p>
+        <div className=" flex justify-center m-6">
+          <ButtonPrimary title="Mehr sehen" size="sm" color="bg-indigo-700" customHover="bg-indigo-600" />
+        </div>
       </div>
     </div>
   );
