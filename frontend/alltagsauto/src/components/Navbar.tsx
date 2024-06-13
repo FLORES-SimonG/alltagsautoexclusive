c46d512c9748d33b6405cbafcf2d618333dd9bb8
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-13 15:28:09 +0200

    added Anmelden

--- frontend/alltagsauto/src/components/Navbar.tsx
+++ frontend/alltagsauto/src/components/Navbar.tsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 
-
-
 export default function Navbar() {
-    return (
-        <nav className="bg-gray-800 px-8 py-4">
-        <Image src="/logo.svg" alt="logo" width={200} height={200} />
-      </nav>
-    );
-    }
+  return (
+    <nav className="bg-gray-800 px-8 py-3 flex flex-row justify-between items-center">
+      <Image src="/logo.svg" alt="logo" width={200} height={200} />
+      <ul className="flex justify-center gap-10">
+        <li>
+          <a href="#" className="text-white">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-white">
+            Über uns
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-white">
+            Autos
+          </a>
+        </li>
+        {/* <li>
+            <a href="#" className="text-white">
+              Contact
+            </a>
+          </li> */}
+      </ul>
+      <button className="text-white flex py-3 px-10 justify-center items-center gap-8 border-white border-2 rounded-lg">Anmelden</button>
+    </nav>
+  );
+}
