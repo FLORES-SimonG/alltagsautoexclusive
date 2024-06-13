9b3d22337d3c415ff001a6625d00e2d95646bd69
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-13 20:06:43 +0200

    responsive was not working with Vercel, i had to modify

--- frontend/alltagsauto/src/components/Navbar.tsx
+++ frontend/alltagsauto/src/components/Navbar.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 px-8 py-3 flex flex-row justify-between items-center">
+    <nav className="bg-gray-800 px-8 py-3 flex flex-row justify-center lg:justify-between items-center">
       <a href="#">
-
-      <Image src="/logo.svg" alt="logo" width={200} height={200} />
+        <Image src="/logo.svg" alt="logo" width={200} height={200} />
       </a>
-      <ul className="flex justify-center gap-10">
+      <ul className="hidden lg:flex justify-center gap-10">
         <li>
           <a
             href="#"
@@ -38,7 +37,8 @@ export default function Navbar() {
             </a>
           </li> */}
       </ul>
-      <button className="text-white flex py-3 px-10 justify-center items-center gap-8 border-white border-2 rounded-lg hover:border-tertiary transition-colors">
+      
+      <button className="text-white hidden lg:flex py-3 px-10 justify-center items-center gap-8 border-white border-2 rounded-lg hover:border-tertiary transition-colors">
         Anmelden
       </button>
     </nav>
