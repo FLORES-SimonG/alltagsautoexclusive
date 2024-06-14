0475bf77361304a32ec8147e25da976e412a699a
ValentinoJC <80430338+ValentinoJCarrero@users.noreply.github.com>
2024-06-14 11:29:19 -0300

    Base de datos inicializada

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('dbConfig'),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
