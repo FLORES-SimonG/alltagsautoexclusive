ceb1a989771efbc894fe9d8cd407fadf13bb7e3a
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-25 11:31:26 +0200

    CarProduct v2.0

--- frontend/alltagsauto/src/app/store/[...carDetails]/page.tsx
+++ frontend/alltagsauto/src/app/store/[...carDetails]/page.tsx
@@ -1,32 +1,11 @@
+// 'use client'
 import InProgress from "./../../../components/InProgress";
 
-interface CarDetailsProps {
-  id: number;
-}
-
-
-export default function carDetails({ id }: CarDetailsProps) {
-    // let id = CarDetailsProps.id;
-    // console.log(id);
-  if (id) {
-    return (
-        <div>
-            {/* <h2>Sie haben die ID {id} gewählt</h2> */}
-
-      <div className="h-screen w-full flex flex-col justify-center items-center ">
-        <InProgress />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-        <div>
-        {/* <h2>Sie haben die ID --{id} gewählt</h2> */}
-
-  <div className="h-screen w-full flex flex-col justify-center items-center ">
-    <InProgress />
+export default function carDetails() {
+  return (
+    <div>
+      <h1>Car Details</h1>
+      <InProgress />
     </div>
-  </div>
-    );
-  }
+  );
 }
