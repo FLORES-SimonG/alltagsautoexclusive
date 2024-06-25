747f76eab2036cfd38920792b3077c970de31716
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-25 11:13:40 +0200

    buttons conected

---
+++ frontend/alltagsauto/src/app/rentalProposal/page.tsx
@@ -0,0 +1,11 @@
+import InProgress from './../../components/InProgress';
+
+
+export default function RentalProposal(){
+
+    return (
+        <div className='h-screen w-full flex flex-col justify-center items-center '>
+            <InProgress/>
+        </div>
+    )
+}
---
+++ frontend/alltagsauto/src/app/store/[...carDetails]/page.tsx
@@ -0,0 +1,32 @@
+import InProgress from "./../../../components/InProgress";
+
+interface CarDetailsProps {
+  id: number;
+}
+
+
+export default function carDetails({ id }: CarDetailsProps) {
+    // let id = CarDetailsProps.id;
+    // console.log(id);
+  if (id) {
+    return (
+        <div>
+            {/* <h2>Sie haben die ID {id} gewählt</h2> */}
+
+      <div className="h-screen w-full flex flex-col justify-center items-center ">
+        <InProgress />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+        <div>
+        <h2>Sie haben die ID --{id} gewählt</h2>
+
+  <div className="h-screen w-full flex flex-col justify-center items-center ">
+    <InProgress />
+    </div>
+  </div>
+    );
+  }
+}
