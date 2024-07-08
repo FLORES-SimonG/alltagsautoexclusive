0ab5753afc4c4eee25cd36f968377089b34cce88
Miguel-Angel-tapia <tapiamiguelybr@gmail.com>
2024-07-08 10:13:04 -0300

    Cars module

---
+++ back/src/entities/Car.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'Cars' })
+export class Car {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  seatingCapacity: number;
+
+  @Column()
+  doorsNumber: number;
+
+  @Column()
+  fuelType: string;
+
+  @Column()
+  transmissionType: string;
+
+  @Column()
+  price: number;
+}
