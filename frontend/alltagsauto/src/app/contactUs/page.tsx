607b230df93639c8d928e8cb9eb3d75a235e91ff
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-07-13 21:59:26 +0200

    new items in Nav with pages

---
+++ frontend/alltagsauto/src/app/contactUs/page.tsx
@@ -0,0 +1,13 @@
+import InProgress from './../../components/InProgress';
+
+
+
+export default function ContactUs(){
+
+    return (
+        <div className='h-screen w-full flex flex-col justify-center items-center '>
+            
+            <InProgress/>
+        </div>
+    )
+}
